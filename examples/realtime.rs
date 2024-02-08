8f79d1f749762c6f518688114e8c1ea7a111b2ca
Paul Buser <buser.paul@gmail.com>
2024-02-08 17:38:56 -0400

    Delete tests and benchmarks

--- examples/realtime.rs
+++ examples/realtime.rs
@@ -31,7 +31,7 @@ fn run<T: Sample>(device: &Device, config: &StreamConfig) {
 }
 
 fn write_input_data<T: Sample>(input: &[T], yin: &yin::Yin) {
-    let f64_vals: Vec<f64> = input.iter().map(|x| x.to_f32() as f64).collect();
-    let estimate = yin.estimate_freq(&f64_vals).unwrap_or(-1.0);
+    let f32_vals: Vec<f32> = input.iter().map(|x| x.to_f32()).collect();
+    let estimate = yin.estimate_freq(&f32_vals).unwrap_or(-1.0);
     println!("Estimated Frequency: {}", estimate);
 }
